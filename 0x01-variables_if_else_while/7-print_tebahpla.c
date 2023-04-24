01127ca52a67b7b89ccabd69404f4e3fc9d28b8f
Berndiam <berniceyaaoffei@gmail.com>
2023-04-24 08:33:34 -0700

    number 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char ch;
+        char ch;
 
-	for (ch = 'Z'; ch >= 'a'; ch--)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+        for (ch = 'Z'; ch >= 'a'; ch--)
+        {
+                putchar(ch);
+        }
+        putchar('\n');
+        return (0);
 }
