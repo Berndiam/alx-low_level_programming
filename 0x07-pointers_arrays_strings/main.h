f765f1ad2bc39dae25f609576ae4070ca9b4dc47
Berndiam <berniceyaaoffei@gmail.com>
2023-05-08 06:20:39 -0700

    corrected

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,4 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
-int _putchar(int c);
 #endif
